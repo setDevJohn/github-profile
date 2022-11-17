4756c9722d905d9ed7c13da93b713379726907c1
Jhony Freitas <jhony00._@hotmail.com>
2022-11-16 21:25:06 -0300

    Atualização de teste do local dos arquivos

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
-import { getUser } from '/src/js/services/user.js'
-import { getRepositories } from "/src/js/services/repositories.js"
-import { getEvents } from "/src/js/services/events.js"
+import { getUser } from './services/user.js'
+import { getRepositories } from "./services/repositories.js"
+import { getEvents } from "./services/events.js"
 
-import { user } from '/src/js/objects/user.js'
-import { screen } from '/src/js/objects/screen.js'
+import { user } from './objects/user.js'
+import { screen } from './objects/screen.js'
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
